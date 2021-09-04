c464927f7f98e94a5add8312baa10eca0151967a
Yoon <xoals4340@hs.ac.kr>
2021-09-04 22:19:41 +0900

    config refactoring

--- spring_basic/src/main/java/hello/core/AppConfig.java
+++ spring_basic/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,11 +13,20 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {  //공연 기획자 => 배우들의 섭외를 여기서..
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());  //객체 생성 -> 생성자 파라미터로 Memory멤버리파지토리가 들어감.
+        return new MemberServiceImpl(memberRepository());  //객체 생성 -> 생성자 파라미터로 Memory멤버리파지토리가 들어감.
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();        //역할
+    }
+
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();             //역할
+        return new RateDiscountPolicy();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
 }
--- spring_basic/src/test/java/hello/core/order/OrderServiceTest.java
+++ spring_basic/src/test/java/hello/core/order/OrderServiceTest.java
@@ -14,6 +14,7 @@ public class OrderServiceTest {
     MemberService memberService;
     OrderService orderService;
 
+
     @BeforeEach
     public void beforeEach(){
         AppConfig appConfig = new AppConfig();
