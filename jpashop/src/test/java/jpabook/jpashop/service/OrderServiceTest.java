da7dd2f3c5af4a52dfb405ae4d73668ef9b1ddcd
Yoon <xoals4340@hs.ac.kr>
2021-12-31 10:57:13 +0900

    order test complete

--- jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -33,20 +34,13 @@ class OrderServiceTest {
     public void 상품주문() {
 
         //given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울","강가","123-123"));
-        em.persist(member);
-
-        Item book = new Book();
-        book.setName("시골 jpa");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Member member = createMember();
+        Book book = createBook();
 
         //when
         int orderCount=2;
         Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
         //then
         Order getOrder = orderRepository.findOne(orderId);
         assertThat(getOrder.getStatus()).isEqualTo(OrderStatus.ORDER);
@@ -57,12 +51,48 @@ class OrderServiceTest {
     }
 
     @Test
-    public void 주문취소() {
+    public void 상품주문_재고수량초과() {
+        Member member = createMember();
+        Book book = createBook();
+
+        int orderCount=11;
+        org.junit.jupiter.api.Assertions.assertThrows(
+                NotEnoughStockException.class, () -> orderService.order(member.getId(), book.getId(), orderCount)
+        );
 
     }
 
     @Test
-    public void 상품주문_재고수량초과() {
+    public void 주문취소() {
+        //given
+        Member member = createMember();
+        Book book = createBook();
+        Long orderId = orderService.order(member.getId(), book.getId(), 2);
 
+        //when
+        orderService.cancel(orderId);
+        Order order = orderRepository.findOne(orderId);
+        //then
+        Assertions.assertThat(OrderStatus.CANCEL).isEqualTo(order.getStatus());
+        Assertions.assertThat(book.getStockQuantity()).isEqualTo(10);
     }
+
+    private Member createMember(){
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    private Book createBook(){
+        Book book = new Book();
+        book.setName("시골 jpa");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+        return book;
+    }
+
+
 }
