daf2b049fe7cb982d4c444ff673a43501ab29723
Yoon <xoals4340@hs.ac.kr>
2022-01-07 00:31:00 +0900

    complete - 1

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -52,9 +53,9 @@ public class OrderService {
         order.cancel();
     }
 
-    //검색
-//    public List<Order> findOrder(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+
+   public List<Order> findOrders(OrderSearch orderSearch) {
+       return orderRepository.findAllByCriteria(orderSearch);
+    }
 
 }
