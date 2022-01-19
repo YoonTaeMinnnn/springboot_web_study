e583456df83d6eac9d241aadcd0674b3a8d17fa6
Yoon <xoals4340@hs.ac.kr>
2022-01-19 17:22:56 +0900

    bean validation 준비

---
+++ validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,31 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(100000);
+
+        Set<ConstraintViolation<Item>> validate = validator.validate(item);
+        for (ConstraintViolation<Item> itemConstraintViolation : validate) {
+            System.out.println("itemConstraintViolation = " + itemConstraintViolation);
+            System.out.println("itemConstraintViolation = " + itemConstraintViolation.getMessage());
+        }
+
+    }
+}
