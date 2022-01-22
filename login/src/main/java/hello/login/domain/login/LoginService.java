9561e3b799236a17ef1f3d8a9e44dd4a9d02d502
Yoon <xoals4340@hs.ac.kr>
2022-01-22 11:45:52 +0900

    login service

---
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,33 @@
+package hello.login.domain.login;
+
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+//        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+//        Member member = findMemberOptional.get();
+//        if (member.getPassword().equals(password)) {
+//            return member;
+//        }else{
+//            return null;
+//        }
+
+        return memberRepository.findByLoginId(loginId)
+                .filter( m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+
+
+}
---
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,45 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save member = {}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+//    public Optional<Member> findByLoginId(String loginId){
+//        List<Member> all = findAll();
+//        for (Member member : all) {
+//            if (member.getLoginId().equals(loginId)){
+//                return Optional.of(member);
+//            }
+//        }
+//        return Optional.empty();
+//    }
+
+    public Optional<Member> findByLoginId(String loginId){
+        return findAll().stream().filter(
+                m -> m.getLoginId().equals(loginId)).findAny();
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+}
