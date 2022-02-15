32509dd63bc47c791dc73582363b88a6c32269ec
Yoon <xoals4340@hs.ac.kr>
2022-02-15 23:32:23 +0900

    exceptionResolver

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -2,17 +2,21 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
 
     //@Bean
     public FilterRegistrationBean logFilter() {
@@ -30,5 +34,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
     }
 }
--- exception/src/main/java/hello/exception/api/ApIExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApIExceptionController.java
@@ -18,6 +18,9 @@ public class ApIExceptionController {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
         }
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력값");
+        }
 
         return new MemberDto(id, "hello " + id);
     }
