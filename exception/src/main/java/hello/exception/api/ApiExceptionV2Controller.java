7238adaa56e765bd9f1d24262a70fea9535ba4ea
Yoon <xoals4340@hs.ac.kr>
2022-03-07 22:49:51 +0900

    exceptionHandler

---
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -0,0 +1,63 @@
+package hello.exception.api;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class ApiExceptionV2Controller {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  //IllegalArgumentException, UserException을 제외한 예외는 여기서!
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exception] ex", e);
+        return new ErrorResult("EX", e.getMessage());
+    }
+
+    @GetMapping("/api2/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력값");
+        }
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/api2/members/bind")
+    public Integer bind(@RequestParam Integer data) {
+        return data;
+    }
+
+    @Data  //requiredArgsConstructor, getter, setter, toString, toHashCode
+    @AllArgsConstructor  //모든 필드를 매개변수로 받는 생성자 생성
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
--- exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
+++ exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -4,6 +4,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.UUID;
 
 @Slf4j
