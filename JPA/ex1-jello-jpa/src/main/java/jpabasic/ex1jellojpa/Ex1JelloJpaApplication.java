9312b4eff8ca884283d2c2a756ce324009101e77
Yoon <xoals4340@hs.ac.kr>
2022-03-18 21:50:40 +0900

    new prj

---
+++ JPA/ex1-jello-jpa/src/main/java/jpabasic/ex1jellojpa/Ex1JelloJpaApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.ex1jellojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1JelloJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1JelloJpaApplication.class, args);
+	}
+
+}
---
+++ JPA/ex1-jello-jpa/src/test/java/jpabasic/ex1jellojpa/Ex1JelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.ex1jellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1JelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
