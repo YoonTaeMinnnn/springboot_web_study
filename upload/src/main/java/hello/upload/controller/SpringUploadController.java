a4bc7ed271ea058eca2e89beaa2821b6ae32daf3
Yoon <xoals4340@hs.ac.kr>
2022-04-04 21:15:23 +0900

    [파일저장]

---
+++ upload/src/main/java/hello/upload/controller/SpringUploadController.java
@@ -0,0 +1,56 @@
+package hello.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/spring")
+public class SpringUploadController {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFileV1(@RequestParam String itemName,
+                             @RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
+
+        log.info("request={}", request);
+        log.info("itemName={}", itemName);
+        log.info("multipartFile={}", file);
+
+        if (!file.isEmpty()) {
+            String fullPath = fileDir + file.getOriginalFilename();
+            log.info("파일 저장 fullPath = {}", fullPath);
+            file.transferTo(new File(fullPath));  //파일 저장
+        }
+        return "upload-form";
+
+    }
+
+
+
+
+}
