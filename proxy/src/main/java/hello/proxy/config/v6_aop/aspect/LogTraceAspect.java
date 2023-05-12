8284ebb40bfa63dab632f59f4381db1b1c237aae
YoonTaeminnnn <xoals4340@hs.ac.kr>
2023-05-12 16:21:50 +0900

    @Aspect

---
+++ proxy/src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,38 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+
+@Slf4j
+@Aspect
+@RequiredArgsConstructor
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    // 스프링이 어드바이저를 생성
+    @Around("execution(* hello.proxy.app..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            // 로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
