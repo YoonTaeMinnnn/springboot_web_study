c1db52befb6d71d92b6f418e830a502078af964e
YoonTaeminnnn <xoals4340@hs.ac.kr>
2023-05-31 00:33:48 +0900

    db-create

--- proxy/src/test/java/hello/proxy/app/v1/OrderServiceV1ImplTest.java
+++ proxy/src/test/java/hello/proxy/app/v1/OrderServiceV1ImplTest.java
@@ -1,4 +1,23 @@
+package hello.proxy.app.v1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
 class OrderServiceV1ImplTest {
-  
+
+    @Autowired
+    OrderServiceV1 orderServiceV1;
+
+    @Test
+    void test() {
+        log.info("orderServiceV1 = {}", orderServiceV1.getClass());
+
+    }
+
 }
